3948d6363a261adae86cf799549a2e02a5301982
PeterGion <Peter.j.Gionfriddo@gmail.com>
2022-11-17 23:22:44 -0500

    created functions to click on each job listing, however there is a bug with selecting certain jobs

--- jobScraper with functions.py
+++ jobScraper with functions.py
@@ -1,9 +1,11 @@
 #cd C:\Users\Peter\"OneDrive - Eastern Connecticut State University"\"senior resarch"\Scraper
+#git push -u origin master
+#git commit -a -m ""
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 import time
 import pandas as pd
 import matplotlib.pyplot as plot
@@ -16,6 +18,7 @@ codingLanguages = None
 langaugeCount = None
 
 def main():
+    #path to my driver
     driver = webdriver.Chrome(executable_path='C:\\Users\\Peter\\OneDrive - Eastern Connecticut State University\\senior resarch\\Scraper\\chromedriver.exe')
     globals()['codingLanguages'] = [
         'Python',
@@ -30,59 +33,85 @@ def main():
         'Kotlin',
         'Assembly',
     ]
+    #initializing langaugeCount so that the length is known
     globals()['langaugeCount'] = [0,0,0,0,0,0,0,0,0,0,0]
     driver.get(intialLink)
-    
-    
+    #checking if the last page element is there
     lastPageElement = driver.find_elements(By.CSS_SELECTOR,".css-jbuxu0 > div:last-child > a[aria-label=\"Next Page\"]")
-    
-    while(len(lastPageElement) != 0):
-        time.sleep(3)
-        #all other logic will go here
-        #jobPannels = driver.find_elements(By.CLASS_NAME,"jobsearch-ResultsList > li")
-        
-        #this is not going to be here in the final version
-        ActionChains(driver).move_to_element(lastPageElement[0]).perform()
-        time.sleep(1)
-        lastPageElement[0].click()
-        lastPageElement = driver.find_elements(By.CSS_SELECTOR,".css-jbuxu0 > div:last-child > a[aria-label=\"Next Page\"]")
+    jobPannels = driver.find_elements(By.CLASS_NAME,"jobsearch-ResultsList > li")
+    startAtJobListing(driver,jobPannels, 10)
 
-    # print(len(driver.find_elements(By.CSS_SELECTOR,".css-jbuxu0 > div:last-child > a[aria-label=\"Next Page\"]")))
-    #onlyTwo(driver, jobAnchors)
     #create_plot()
 
 
-def onlyTwo(driver,jobPannels):
-    for i in range(4):
-        time.sleep(5)
+def goThoughSetPages(driver, element, amount):
+    iteration = 0
+    #while there is a last page element and it is not higher than the iteration amount
+    while(len(element) != 0 and iteration < amount):
+        time.sleep(3)
+        #all other logic will go here
+        #the way that indeed is set up is that they have all of the jobs in clickable divs
+        jobPannels = driver.find_elements(By.CLASS_NAME,"jobsearch-ResultsList > li")
+        #going though all of the job listings on the page
+        allJobListings(driver,jobPannels)
+        print('this is an iteration')
+        # ActionChains(driver).move_to_element(element[0]).perform()
+        time.sleep(1)
+        #clicks on the next page element when the page is finished
+        element[0].click()
+        #checks to see if  the next page eleement is there so when it goes to the loop condtional it can decided if it should continue
+        element = driver.find_elements(By.CSS_SELECTOR,".css-jbuxu0 > div:last-child > a[aria-label=\"Next Page\"]")
+        iteration += 1
+
+
+def goThoughPages(driver, element):
+    #while there is a last page element and it is not higher than the iteration amount
+    while(len(element) != 0):
+        time.sleep(3)
+        #all other logic will go here
+        jobPannels = driver.find_elements(By.CLASS_NAME,"jobsearch-ResultsList > li")
+        allJobListings(driver,jobPannels)
+        # ActionChains(driver).move_to_element(element[0]).perform()
+        time.sleep(1)
+        element[0].click()
+        element = driver.find_elements(By.CSS_SELECTOR,".css-jbuxu0 > div:last-child > a[aria-label=\"Next Page\"]")
+        
+
+def startAtJobListing(driver, jobPannels, jobListingNumber):
+    for i in range(jobListingNumber, len(jobPannels)):
+        print(jobPannels[i].tag_name)
         ActionChains(driver).move_to_element(jobPannels[i]).perform()
+        wait = WebDriverWait(driver, 15)
+        wait.until(EC.element_to_be_clickable(jobPannels[i]))
+        #time.sleep(1)
+        jobPannels[i].click()
+        #time.sleep(5)
+        #jobDescription = findJobDescription(driver)
+        #find_Languages(jobDescription)
+
+        
+def jobListingsSetAmt(driver,jobPannels, amount):
+    for i in range(amount):
         time.sleep(5)
+        # ActionChains(driver).move_to_element(jobPannels[i]).perform()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(jobPannels[i]))
         jobPannels[i].click()
         time.sleep(3)
         jobDescription = findJobDescription(driver)
         find_Languages(jobDescription)
+        
 
 
-def allJobListings(driver, jobPannels):
+def allJobListings(driver,jobPannels):
     for job in jobPannels:
-        if len(job.find_elements(By.ID,'mosaic-afterFifthJobResult')) == 0:
-            time.sleep(5)
-            ActionChains(driver).move_to_element(job).perform()
-            time.sleep(1)
-            job.click()
-            #job = wait.until(EC.invisibility_of_element_located(job))
-            #job = wait.until(EC.element_to_be_clickable(job))
-            #job.click()
-            #jobDescription = findJobDescription(driver)
-            #find_Languages(jobDescription)
-            #create_plot()
-            time.sleep(5)
-
-
-def findJobAnchors():
-    jobAnchors =  globals()['driver'].find_elements(By.CLASS_NAME,"jcs-JobTitle")
-    print(len(jobAnchors))
-    return jobAnchors
+        time.sleep(5)
+        ActionChains(driver).move_to_element(job).perform()
+        time.sleep(5)
+        print(job.text)
+        job.click()
+        time.sleep(3)
+        jobDescription = findJobDescription(driver)
+        find_Languages(jobDescription)
 
 
 def findJobDescription(driver):
@@ -100,12 +129,12 @@ def find_Languages(element):
         globals()['langaugeCount'][0] += 1
     if(elementText.find('Javascript') != -1 or elementText.find('javascript') != -1 or elementText.find('JavaScript') != -1) == True:
         globals()['langaugeCount'][1] += 1
-        print('this is going off')
+        #print('this is going off')
     #looking for 'java' captialized or lowercased and return true if it doesn't return -1
     if((elementText.find('Java') != -1) or (elementText.find('java') != -1) == True and
     (elementText.find('Script',elementText.find('Java')) != -1) or (elementText.find('script',elementText.find('java')) != -1) == True):
     #looking to find 'script' after the word java in lowercase and uppercase
-        print('adding to java')
+        #print('adding to java')
         globals()['langaugeCount'][2] += 1
     if(elementText.find('C/C++')!= -1) == True:
         globals()['langaugeCount'][3] += 1
@@ -123,48 +152,8 @@ def find_Languages(element):
         globals()['langaugeCount'][9] += 1
     if(elementText.find('Assembly')!= -1) == True:
         globals()['langaugeCount'][10] += 1
-    print('this is in find_Languages ' + str(langaugeCount))
+    #print('this is in find_Languages ' + str(langaugeCount))
 
-    
-def find_LanguagesOG(element):
-    print('gets to find_languages')
-    #print(element.text)
-    elements = element.find_elements(By.TAG_NAME,"p,ul")
-    #print(element.text)
-    time.sleep(1)
-    for tagElement in elements:
-        print('started for loop')
-        elementText = tagElement.text
-        #print('got this far')
-        #print(element.text)
-        if(elementText.find('Python')!= -1 or elementText.find('python')!= -1) == True:
-            globals()['langaugeCount'][0] += 1
-        if(elementText.find('Javascript') != -1 or elementText.find('javascript') != -1 or elementText.find('JavaScript') != -1) == True:
-            globals()['langaugeCount'][1] += 1
-            print('this is going off')
-        #looking for 'java' captialized or lowercased and return true if it doesn't return -1
-        if((elementText.find('Java') != -1) or (elementText.find('java') != -1) == True and
-        (elementText.find('Script',elementText.find('Java')) != -1) or (elementText.find('script',elementText.find('java')) != -1) == True):
-        #looking to find 'script' after the word java in lowercase and uppercase
-            print('adding to java')
-            globals()['langaugeCount'][2] += 1
-        if(elementText.find('C/C++')!= -1) == True:
-            globals()['langaugeCount'][3] += 1
-        if(elementText.find('C#')!= -1) == True:
-            globals()['langaugeCount'][4] += 1
-        if(elementText.find('SQL')!= -1) == True:
-            globals()['langaugeCount'][5] += 1
-        if(elementText.find('PHP')!= -1) == True:
-            globals()['langaugeCount'][6] += 1
-        if(elementText.find('Matlab')!= -1) == True:
-            globals()['langaugeCount'][7] += 1
-        if(elementText.find('Swift')!= -1) == True:
-            globals()['langaugeCount'][8] += 1
-        if(elementText.find('Kotlin')!= -1) == True:
-            globals()['langaugeCount'][9] += 1
-        if(elementText.find('Assembly')!= -1) == True:
-            globals()['langaugeCount'][10] += 1
-    print('this is in find_Languages ' + str(langaugeCount))
 
 def create_plot():
     df = pd.DataFrame(data = {"languages":codingLanguages, "Number":langaugeCount})
