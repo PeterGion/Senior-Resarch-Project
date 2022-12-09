9e4cf1b06bc8fbd78751cb3d7cab695dfdbda2bb
PeterGion <Peter.j.Gionfriddo@gmail.com>
2022-12-08 20:57:40 -0500

    updated create_plot to include changes found in test pi plot and general spiffing up

--- jobScraper with functions.py
+++ jobScraper with functions.py
@@ -9,6 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plot
 import csv
 from random import randint
+from tabulate import tabulate
 
 'https://www.indeed.com/jobs?q=software+engineer&l=Connecticut&vjk=d2a438c96f6e9c7e&from=gnav-util-jobsearch--indeedmobile'
 #last page url 'https://www.indeed.com/jobs?q=software+engineer&l=Connecticut&start=480&pp=gQLQAAABhIeCedIAAAAB7hgVOgC7AQQBJ2oGDgc-BgQGOajjWXbVPKtoxz6gCwrUiOulV1DE8uIWhUoN96a_OuBT8U_7MVbePCJ-LGFcdnO782Xn_FHn_kyCyXzXzUApJebFu3ZPLcBJ7fR3OAKkzXXF7yin61pkBkXh0NtyPYQkbIV6AQywYLj9_Svtj_2y5YhVU1Ak0d7D0YfDB32euOSKGPhbdg1WLeCKNO8mN10bZM35cPeLX_vBSufZLhQmk3ygIaMFnrjAdYemIt0iIAAA&vjk=58cab7537268f79a'
@@ -36,13 +37,15 @@ def main():
     ]
     #initializing langaugeCount so that the length is known
     globals()['langaugeCount'] = [0,0,0,0,0,0,0,0,0,0,0]
+    #globals()['langaugeCount'] = [0,3,0,4,5,6,7,3,4,0,0]
     driver.get(intialLink)
     #checking if the last page element is there
     lastPageElement = driver.find_elements(By.CSS_SELECTOR,".css-jbuxu0 > div:last-child > a[aria-label=\"Next Page\"]")
-    jobPannels = driver.find_elements(By.CLASS_NAME,"jobsearch-ResultsList > li")
-    goThoughSetPages(driver, jobPannels, lastPageElement, 1)
-
-    print(globals()['langaugeCount'])
+    #jobPannels = driver.find_elements(By.CLASS_NAME,"jobsearch-ResultsList > li")
+    #goThoughSetPages(driver, jobPannels, lastPageElement, 1)
+    goThoughPages(driver, lastPageElement)
+    #print(globals()['langaugeCount'])
+    #run this to make sure it works
     create_plot()
 
 
@@ -81,27 +84,14 @@ def goThoughSetPages(driver, jobPannels, lastPageElement, amount):
 def goThoughPages(driver, element):
     #while there is a last page element and it is not higher than the iteration amount
     while(len(element) != 0):
-        time.sleep(3)
+        time.sleep(generateSeconds())
         #all other logic will go here
         jobPannels = driver.find_elements(By.CLASS_NAME,"jobsearch-ResultsList > li")
         allJobListings(driver,jobPannels)
-        # ActionChains(driver).move_to_element(element[0]).perform()
-        time.sleep(1)
+        ActionChains(driver).move_to_element(element[0]).perform()
+        time.sleep(generateSeconds())
         element[0].click()
         element = driver.find_elements(By.CSS_SELECTOR,".css-jbuxu0 > div:last-child > a[aria-label=\"Next Page\"]")
-        
-
-def startAtJobListing(driver, jobPannels, jobListingNumber):
-    for i in range(jobListingNumber, len(jobPannels)):
-        print(jobPannels[i].tag_name)
-        ActionChains(driver).move_to_element(jobPannels[i]).perform()
-        # wait = WebDriverWait(driver, 15)
-        # wait.until(EC.element_to_be_clickable(jobPannels[i]))
-        time.sleep(1)
-        jobPannels[i].click()
-        #time.sleep(5)
-        #jobDescription = findJobDescription(driver)
-        #find_Languages(jobDescription)
         
 
 def allJobListings(driver,jobPannels):
@@ -109,24 +99,23 @@ def allJobListings(driver,jobPannels):
         if(len(job.find_elements(By.CSS_SELECTOR,"#mosaic-afterFifthJobResult")) == 0 and 
         len(job.find_elements(By.CSS_SELECTOR,"#mosaic-afterTenthJobResult")) == 0 and
         len(job.find_elements(By.CSS_SELECTOR,"#mosaic-afterFifteenthJobResult")) == 0):
-            time.sleep(1)
+            time.sleep(generateSeconds())
             ActionChains(driver).move_to_element(job).perform()
-            time.sleep(1)
+            time.sleep(generateSeconds())
             #print(job.text)
             job.click()
-            time.sleep(3)
+            time.sleep(generateSeconds())
             jobDescription = findJobDescription(driver)
             find_Languages(jobDescription)
             globals()['jobCount'] += 1
             updateCsv()
 
 
-
 def findJobDescription(driver):
     #class jcs-JobTitle
     #jobDescription = seleniumDriver.find_element(By.CLASS_NAME,"jobsearch-jobDescriptionText")
     jobDescription = driver.find_element(By.CLASS_NAME,"jobsearch-jobDescriptionText")
-    time.sleep(1)
+    time.sleep(generateSeconds())
     ActionChains(driver).move_to_element(jobDescription).perform()
     return jobDescription
 
@@ -161,13 +150,35 @@ def find_Languages(element):
         globals()['langaugeCount'][10] += 1
     #print('this is in find_Languages ' + str(langaugeCount))
 
-
+#run this to make sure it works
 def create_plot():
-    df = pd.DataFrame(data = {"languages":codingLanguages, "Number":langaugeCount})
-    plt = df.plot.barh(x = "languages", y = "Number", title = "Languages from Indeed",legend = False)
-    #plt = df.plot.barh(rot = 0,legend = False)
-    plt.set_xlabel("Amount")
-    plt.set_ylabel("Languages")
+    # df = pd.DataFrame(data = {"languages":codingLanguages, "Number":langaugeCount})
+    # plt = df.plot.bar(x = "languages", y = "Number", title = "Languages from Indeed",legend = False)
+    # #plt = df.plot.barh(rot = 0,legend = False)
+    # plt.set_xlabel("Amount")
+    # plt.set_ylabel("Languages")
+    # plot.show(block=True)
+
+    df = pd.DataFrame(data = {"Languages":globals()['codingLanguages'], "Amount":globals()['langaugeCount']})
+
+    #globals()['jobCount'] = 15
+    percentages = [0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00]
+    for index in range(len(df)):
+        #print(type(percentages[index]))
+        percentages[index] = df['Amount'][index] / globals()['jobCount'] * 100
+        if(percentages[index] != 0):
+            percentages[index] = "{:.2f}%".format(percentages[index])
+        else:
+            percentages[index] = "{:.0f}%".format(percentages[index])
+    print(percentages)
+    df['%'] = percentages
+
+    plt = df.plot.bar(x = "Languages", y = "Amount", title = "Languages from Indeed",legend = False)
+    
+    for i in range(len(globals()['codingLanguages'])):
+        plt.text(i,globals()['langaugeCount'][i],globals()['langaugeCount'][i],ha = 'center')
+    plt.set_ylabel("Amount")
+    print(tabulate(df, tablefmt='plain-text'))
     plot.show(block=True)
 
 
